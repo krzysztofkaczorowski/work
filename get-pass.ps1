a8de601eb20bccfca9c3d4f2a2bf19da0ba4c9a4
krzysztofkaczorowski <krzysztof.kaczorowski@unit4.com>
2017-05-20 12:32:55 +0200

    get-pass
    password generator

---
+++ get-pass.ps1
@@ -0,0 +1,19 @@
+﻿function get-pass
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [int]
+        $lenght
+    )
+    
+    if($lenght -lt 3){$lenght =15 }
+    $obj= (48..57) + (65..90) + (97..122)
+    $password=$obj|Get-Random -Count $lenght|% {[char]$_}
+    
+    if(!((-join $password) -cmatch '^.*(?=.*\d.*\d)(?=.*[A-Z].*[A-Z]).*$')){
+        get-pass $lenght
+    }
+    else{-join $password}
+}
